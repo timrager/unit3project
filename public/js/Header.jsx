63fc533dc5c31ca758d9ea53f94f6f56e19f4938
timrager <tim.rager@gmail.com>
2020-05-29 10:16:36 -0700

    added a blank controller (oxygen.js), blank models (plants.js, user.js)

--- public/js/Header.jsx
+++ public/js/Header.jsx
@@ -7,6 +7,7 @@ class Header extends React.Component{
         return(
 
             <header>
+                <h1> TEST </h1>
                 <div id="logo"></div>
                 <div id="avatar"></div>
                 <div id="cart"></div>
