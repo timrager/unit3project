9e685fdfe9b83c016c24f58306f3fca35a9314ef
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-05-29 20:26:08 -0400

    Maybe good now?

--- controllers/oxygen.js
+++ controllers/oxygen.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-// Import Model
-// const Plants = require('../models/pants.js');
-// const Users = require('../models/users.js');
-// For when we have the models set up
-
-// Index Route
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-=======
-=======
->>>>>>> f6c88306ed5c9ebfc0732de52a6d7eea5bc15988
 //=======================
 // DEPENDENCIES
 //=======================
@@ -35,10 +18,3 @@ router.get('/', (req,res) => {
         res.json(foundPlants);
     })
 })
-
-<<<<<<< HEAD
-module.exports = router;
->>>>>>> 3ea546066bb52c9ea36168d85275d55ee291ef7b
-=======
-module.exports = router;
->>>>>>> f6c88306ed5c9ebfc0732de52a6d7eea5bc15988
