316ffaa365137fa560cb7fa4a63c3150ffbb6ba7
Katie <katie@katies-air.lan>
2020-05-31 11:26:26 -0400

    adding footer tag to footer

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -4,12 +4,14 @@ class Footer extends React.Component {
     render () {
         return (
             <div>
+                <footer>
                 <h1>Finding my footing</h1>
                 {/* Images Coming Soon */}
                 <a href="#">Instagram</a>
                 <a href="#">Pintrest</a>
                 <a href="#">Facebook</a>
                 <a href="#">Twitter</a>
+                </footer>
             </div>
         )
     }
