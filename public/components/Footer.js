39cebbbc98402bde3d82513968394bda36d55142
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-05-31 15:14:51 -0400

    Changed class to className on the icon tags

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -3,12 +3,12 @@
 class Footer extends React.Component {
     render () {
         return (
-            <div className="footer">
+            <footer className="footer">
                 <a href="#"><i className="fab fa-facebook-square"></i></a>
-                <a href="#"><i class="fab fa-instagram-square"></i></a>
-                <a href="#"><i class="fab fa-pinterest-square"></i></a>
-                <a href="#"><i class="fab fa-twitter-square"></i></a>
-            </div>
+                <a href="#"><i className="fab fa-instagram-square"></i></a>
+                <a href="#"><i className="fab fa-pinterest-square"></i></a>
+                <a href="#"><i className="fab fa-twitter-square"></i></a>
+            </footer>
         )
     }
 }
