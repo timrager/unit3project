6b7fbdcf556c1106ec57b7c6fb5f456169df0be8
Katie <katie@katies-air.lan>
2020-05-31 12:52:42 -0400

    styling header buttons

--- public/components/Header.js
+++ public/components/Header.js
@@ -5,10 +5,12 @@ class Header extends React.Component{
 
         return(
             <header>
-                <div id="logo" className="headerButton">Oxygen</div>
-                <button id="avatar" className="headerButton">Profile</button>
-                <button id="cart" className="headerButton">Cart</button>
-                <button id="hamburger" className="headerButton">Menu</button>
+                <div id="logo">Oxygen</div>
+                <div className="buttonContainer">
+                    <button id="avatar" className="headerButton">Profile</button>
+                    <button id="cart" className="headerButton">Cart</button>
+                    <button id="hamburger" className="headerButton">Menu</button>
+                </div>
             </header>
         )
 
