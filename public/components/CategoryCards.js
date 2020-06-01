d430b6b37a90bee1d41a74a0ca85b03401b26fd4
Katie <katie@katies-air.lan>
2020-05-31 21:43:03 -0400

    Category Cards created

---
+++ public/components/CategoryCards.js
@@ -0,0 +1,14 @@
+class Category extends React.Component {
+
+    render() {
+        
+        return(
+            <div className="categoryContainer">
+                <button className="pet">Pet Friendly</button>
+                <button className="air">Air Purifying</button>
+                <button className="low">Low Maintenance</button>
+                <button className="mood">Mood Boosting</button>
+            </div>
+        )
+    }
+}
