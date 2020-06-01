6a98a4845bf494b36577e9e3a0ac6701eb88d7c9
Katie <katie@katies-air.lan>
2020-06-01 17:10:16 -0400

    pulling from dev

--- public/components/CategoryCards.js
+++ public/components/CategoryCards.js
@@ -1,14 +1,14 @@
 class Category extends React.Component {
 
     render() {
-        
+
         return(
             <div className="categoryContainer">
-                <button className="pet">Pet Friendly</button>
-                <button className="air">Air Purifying</button>
-                <button className="low">Low Maintenance</button>
-                <button className="mood">Mood Boosting</button>
+                <button className="plant" id="pet">Pet Friendly</button>
+                <button className="plant" id="air">Air Purifying</button>
+                <button className="plant" id="low">Low Maintenance</button>
+                <button className="plant" id="mood">Mood Boosting</button>
             </div>
         )
     }
-}
+} 
