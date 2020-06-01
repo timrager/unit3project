3c9e6b59db195a51e9719c192305a651e010a5b1
Katie <katie@katies-air.lan>
2020-06-01 18:05:19 -0400

    pulling Leah's Cards.js

--- public/components/PlantCards.js
+++ public/components/PlantCards.js
@@ -1,10 +1,15 @@
 class Plant extends React.Component {
 
+    state = {
+        name: name,
+        category: category
+    }
+
     render() {
 
         return(
             <div className="plantContainer">
-                
+                <button className={this.props.something}></button>
             </div>
         )
     }
