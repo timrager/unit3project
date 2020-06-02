e0153850240a0c38b3702c8e9fa9aadef6227c96
Whitney Smith <whitney.smith.lee@gmail.com>
2020-06-02 14:54:42 -0700

    testing form components

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,31 @@
 
 
+class NewUser extends React.Component {
+    render() {
+        const { handleSignUp, handleFormInput, password, email } = this.props;
+    return(
+        <form onSubmit={handleSignUp}>
+            <input type="text" id="email" value={email} onChange={handleFormInput} />
+            <input type="password" id="password" value={password} onChange={handleFormInput} />
+            <input type="submit"/>
+        </form>
+    )
+    }
+}
+
+class UserLogin extends React.Component {
+    render() {
+        const { handleLogIn, handleFormInput, password, email } = this.props;
+    return(
+        <form onSubmit={handleLogIn}>
+            <input type="text" id="email" value={email} onChange={handleFormInput} />
+            <input type="password" id="password" value={password} onChange={handleFormInput} />
+            <input type="submit"/>
+        </form>
+    )
+    }
+}
+
 class App extends React.Component{
 
     state = {
@@ -75,17 +101,29 @@ class App extends React.Component{
         return(
             <div>
                 <Header />
-                    <form onSubmit={this.handleSignUp}>
+                    {/* <form onSubmit={this.handleSignUp}>
                         <input type="text" id="email" value={this.state.email} onChange={this.handleFormInput} />
                         <input type="password" id="password" value={this.state.password} onChange={this.handleFormInput} />
                         <input type="submit"/>
-                    </form>
-
-                    <form onSubmit={this.handleLoginIn}>
+                    </form> */}
+                    <NewUser
+                        email={this.state.email}
+                        password={this.state.password}
+                        handleFormInput={this.handleFormInput}
+                        handleSignUp={this.handleSignUp}
+                    />
+                    
+                    <UserLogin 
+                        email={this.state.email}
+                        password={this.state.password}
+                        handleLogIn={this.state.handleLogIn}
+                        handleFormInput={this.state.handleFormInput}
+                    />
+                    {/* <form onSubmit={this.handleLoginIn}>
                         <input type="text" id="email" value={this.state.email} onChange={this.handleFormInput} />
                         <input type="password" id="password" value={this.state.password} onChange={this.handleFormInput} />
                         <input type="submit"/>
-                    </form>
+                    </form> */}
                 <Api />
                 {/* <Catcard /> */}
                 <CatCard />
