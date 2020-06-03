80928edc81011d085205f853b55155569de75031
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-03 13:53:32 -0400

    Added a default route and a switch statement. Can render the home page.

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -2,7 +2,7 @@ class CatCard extends React.Component {
     render () {
         return (
             <div className="plantBox">
-                <a href={this.props.image}><img className="plantMenu" src={this.props.image} onClick={} /></a>
+                <a href={this.props.image}><img className="plantMenu" src={this.props.image} /></a>
                 <h3>{this.props.category}</h3>
                 <p>{this.props.info}</p>
             </div>
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // import { BrowserRouter, Route, Link } from "react-router-dom";
-const { BrowserRouter, Link, Route } = ReactRouterDOM;
+const { BrowserRouter, Link, Switch, Route } = ReactRouterDOM;
 
 class App extends React.Component{
 
@@ -142,10 +142,26 @@ class App extends React.Component{
                 <Header user={this.state}/>
                 <BrowserRouter>
                     <nav><Link to ="/testme">TestLink</Link></nav>
-                    <Route path="/testme" 
-                    render={(props) => { 
-                        return ( <Sample1 user={this.state}/> ) }} 
-                    />
+                    <Switch>
+                        <Route path="/testme" 
+                        render={(props) => { 
+                            return ( <Sample1 user={this.state}/> ) }} 
+                        />
+                        {/* Default or Homepage */}
+                        <Route path="/"
+                            render={() => {
+                                return (
+                                    <div>
+                                        <CatCard image="https://www.thespruce.com/thmb/qrWRABcI6K_plsLUn2cX8WS_-QE=/2358x1613/filters:fill(auto,1)/kararileysempervivum-18-crop-565dd32562e34681a627e2de84f691e1.jpg" category="Low Maintenance" info="Don't feel like you have a green thumb? This category of plants is designed for the first time plant owner, or the human that would like to have some plants in their life, but may have not had good luck keeping previous plant babies alive in the past." />
+                                        <CatCard image="https://www.almanac.com/sites/default/files/styles/opengraph/public/image_nodes/african-violet-houseplant.jpg?itok=qiMZjFZs" category="Air Purifying" info="These plants are great for purifying your home and office. If you feel as though your space has become musty and stale, then this category of plant is for you!" />
+                                        <CatCard image="https://www.jacksonandperkins.com/images/xxl/27677.jpg" category="Pet Loving" info="No more worries about which plants are safe for your furry family members. This plant category is all safe for plants to nom on." />
+                                        <CatCard image="https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Pygmy_Date_Palm_450_1_grande.jpg?v=1549683425" category="Mood Boosting" into="If cabin fever is giving you the blues, pick a plant from this category and let the good vibes flow." />
+                                        <Footer />
+                                    </div>
+                                )
+                            }} 
+                        />
+                    </Switch>
                 </BrowserRouter>
                     {/* <UserLogin 
                         email={this.state.email}
@@ -166,9 +182,9 @@ class App extends React.Component{
                         handleSignUp={this.handleSignUp}
                         />
                 <Api />
-                <Plant /> */}
-                {/* <Show /> */}
-                {/* <UserProfile />
+                <Plant />
+                <Show />
+                <UserProfile />
                 <CatCard image="https://www.thespruce.com/thmb/qrWRABcI6K_plsLUn2cX8WS_-QE=/2358x1613/filters:fill(auto,1)/kararileysempervivum-18-crop-565dd32562e34681a627e2de84f691e1.jpg" category="Low Maintenance" info="Don't feel like you have a green thumb? This category of plants is designed for the first time plant owner, or the human that would like to have some plants in their life, but may have not had good luck keeping previous plant babies alive in the past." />
                 <CatCard image="https://www.almanac.com/sites/default/files/styles/opengraph/public/image_nodes/african-violet-houseplant.jpg?itok=qiMZjFZs" category="Air Purifying" info="These plants are great for purifying your home and office. If you feel as though your space has become musty and stale, then this category of plant is for you!" />
                 <CatCard image="https://www.jacksonandperkins.com/images/xxl/27677.jpg" category="Pet Loving" info="No more worries about which plants are safe for your furry family members. This plant category is all safe for plants to nom on." />
