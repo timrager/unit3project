3279c81dddce3240a0cb1db30663ab8d8a89a983
Whitney Smith <whitney.smith.lee@gmail.com>
2020-06-02 18:31:48 -0700

    user login & creation components updated

--- public/components/Header.js
+++ public/components/Header.js
@@ -2,12 +2,18 @@
 class Header extends React.Component{
 
     render(){
+        let welcomeUser;
+        if (this.props.user.isLoggedIn) {
+           welcomeUser = (<button id="avatar" className="headerButton">Welcome back, {this.props.user.name}!</button>)
+        } else {
+           welcomeUser = (<button id="avatar" className="headerButton">Log In</button>)
+        }
 
         return(
             <header>
                 <div id="logo">Oxygen</div>
                 <div className="buttonContainer">
-                    <button id="avatar" className="headerButton">Profile</button>
+                    {welcomeUser}
                     <button id="cart" className="headerButton">Cart</button>
                     <button id="hamburger" className="headerButton">Menu</button>
                 </div>
--- public/js/app.js
+++ public/js/app.js
@@ -34,6 +34,7 @@ class App extends React.Component{
 
     handleLogOut = () => {
         this.setState({
+            name: '',
             email: '',
             password: '',
             isLoggedIn: false
@@ -47,7 +48,8 @@ class App extends React.Component{
         fetch('/user/login', {
             body: JSON.stringify({
                 email: this.state.email,
-                password: this.state.password
+                password: this.state.password,
+                name: this.state.name
             }),
             method: "POST",
             headers: {
@@ -64,10 +66,41 @@ class App extends React.Component{
                     password: ''
                 })
             })
-        
-        
+           
     }
 
+    handleNewUser = (event) => {
+        event.preventDefault();
+
+        fetch('/user/signup', {
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                shippingStreet: this.state.shippingStreet,
+                shippingCity: this.state.shippingCity,
+                shippingState: this.state.shippingState,
+                shippingZip: this.state.shippingZip,
+            })
+            .then(createdUser => {
+                return createdUser.json()
+            })
+            .then(jsonedUser => {
+                this.setState({
+                    isLoggedIn: true,
+                    name: '',
+                    email: '',
+                    password: '',
+                    shippingStreet: '',
+                    shippingCity: '',
+                    shippingState: '',
+                    shippingZip: '',
+                })
+                console.log(jsonedUser);
+            })
+            .catch(error => console.log(error))
+        });
+    }
     handleFormInput = (event) => {
         this.setState({
             [event.target.id]: event.target.value
@@ -75,48 +108,59 @@ class App extends React.Component{
     }
 
     render() {
-        let showForm;
-        if (this.state.isLoggedIn) {
-            showForm = (
-                <UserLogin 
-                    email={this.state.email}
-                    password={this.state.password}
-                    handleLogIn={this.state.handleLogIn}
-                    handleFormInput={this.state.handleFormInput}
-                />
-            )
-        } else {
-            showForm = (
-                <NewUser
-                    email={this.state.email}
-                    password={this.state.password}
-                    handleFormInput={this.handleFormInput}
-                    handleSignUp={this.handleSignUp}
-                />
-            )
-        } 
+        // let showForm;
+        // if (this.state.isLoggedIn) {
+        //     showForm = (
+        //         <UserLogin 
+        //             email={this.state.email}
+        //             password={this.state.password}
+        //             handleLogIn={this.state.handleLogIn}
+        //             handleFormInput={this.state.handleFormInput}
+        //         />
+        //     )
+        // } else {
+        //     showForm = (
+        //         <NewUser
+        //             email={this.state.email}
+        //             password={this.state.password}
+        //             name={this.state.name}
+        //             email={this.state.email}
+        //             password={this.state.password}
+        //             shippingStreet={this.state.shippingStreet}
+        //             shippingCity={this.state.shippingCity}
+        //             shippingState={this.state.shippingState}
+        //             shippingZip={this.state.shippingZip}
+        //             handleFormInput={this.handleFormInput}
+        //             handleSignUp={this.handleSignUp}
+        //         />
+        //     )
+        // } 
         return(
             <div>
-                <Header />
-                    {showForm}
-                    {/* <NewUser
-                        email={this.state.email}
-                        password={this.state.password}
-                        handleFormInput={this.handleFormInput}
-                        handleSignUp={this.handleSignUp}
-                    /> */}
-                    
-                    {/* <UserLogin 
+                <Header user={this.state}/>
+                    <UserLogin 
                         email={this.state.email}
                         password={this.state.password}
                         handleLogIn={this.state.handleLogIn}
                         handleFormInput={this.state.handleFormInput}
-                    /> */}
-                <Api />
+                    />
+                    
+                    <NewUser
+                        name={this.state.name}
+                        email={this.state.email}
+                        password={this.state.password}
+                        shippingStreet={this.state.shippingStreet}
+                        shippingCity={this.state.shippingCity}
+                        shippingState={this.state.shippingState}
+                        shippingZip={this.state.shippingZip}
+                        handleFormInput={this.handleFormInput}
+                        handleSignUp={this.handleSignUp}
+                        />
+                {/* <Api />
                 <CatCard image="https://www.thespruce.com/thmb/qrWRABcI6K_plsLUn2cX8WS_-QE=/2358x1613/filters:fill(auto,1)/kararileysempervivum-18-crop-565dd32562e34681a627e2de84f691e1.jpg" category="Low Maintenance" info="" />
                 <CatCard image="https://www.almanac.com/sites/default/files/styles/opengraph/public/image_nodes/african-violet-houseplant.jpg?itok=qiMZjFZs" category="Air Purifying" info="" />
                 <CatCard image="https://www.jacksonandperkins.com/images/xxl/27677.jpg" category="Pet Loving" info="" />
-                <CatCard image="https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Pygmy_Date_Palm_450_1_grande.jpg?v=1549683425" category="Mood Boosting" into="" />
+                <CatCard image="https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Pygmy_Date_Palm_450_1_grande.jpg?v=1549683425" category="Mood Boosting" into="" /> */}
                 <Footer />
             </div>
         )
