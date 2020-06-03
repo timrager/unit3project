a62146b7a7de8830667b3e6e642a89010d3fb329
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-02 21:40:59 -0400

    Pulled changes added to the dev. Added user profile information to the UserProfile component

--- public/components/UserProfile.js
+++ public/components/UserProfile.js
@@ -1,8 +1,22 @@
 class UserProfile extends React.Component {
+    state = {
+        // do i pull props from something else, or do i actually set state here?
+    }
     render () {
         return (
             <div>
-                
+                <div id="profile">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+                    <p>{this.props.name}</p>
+                    <p>{this.props.email}</p>
+                    <p>{this.props.shippingStreet}</p>
+                    <p>{this.props.shippingCity}</p>
+                    <p>{this.props.shippingState}</p>
+                    <p>{this.props.shippingZip}</p>
+                </div>
+                <div id="cart">
+                    
+                </div>
             </div>
         )
     }
