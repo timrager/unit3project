60acd2217ae895c282cacc15efba2b0da067b058
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-02 23:17:11 -0400

    Pulled new changes from dev and adding my own to my branch

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -1,8 +1,12 @@
 class CatCard extends React.Component {
+    state = {
+        plants: []
+    }
+
     render () {
         return (
             <div className="plantBox">
-                <img className="plantMenu" src={this.props.image} />
+                <a href={this.props.image}><img className="plantMenu" src={this.props.image} onClick={} /></a>
                 <h3>{this.props.category}</h3>
                 <p>{this.props.info}</p>
             </div>
