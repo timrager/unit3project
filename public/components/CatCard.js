059a0ee8827d8f5c38022ba76ba9f455ac44f4ea
Whitney Smith <whitney.smith.lee@gmail.com>
2020-06-03 10:24:22 -0700

    testing data in props

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -1,8 +1,17 @@
+
 class CatCard extends React.Component {
     render () {
+        const { category, plants } = this.props;
+        // console.log(this.props)
+        // console.log(plants);
+        // plants.map((plant) => {
+        //     if (plant.name === "undefined") {
+        //         console.log('error');
+        //     } else (console.log(plant.name));
+        // })
         return (
             <div className="plantBox">
-                <img className="plantMenu" src={this.props.image} />
+               <img className="plantMenu" src={this.props.image} />
                 <h3>{this.props.category}</h3>
                 <p>{this.props.info}</p>
             </div>
--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,23 @@
 class App extends React.Component{
 
     state = {
-        name: '',
-        email: '',
-        password: '',
-        isLoggedIn: false
+        user: {
+            name: '',
+            email: '',
+            password: '',
+            isLoggedIn: false
+        },
+        plants: []
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = (event) => {
+        fetch('/oxygen')
+            .then(response => response.json())
+            .then(data => this.setState({ plants: data }))
     }
 
     handleSignUp = (event) => {
@@ -13,13 +26,13 @@ class App extends React.Component{
 
         fetch('/user/signup', {
             body: JSON.stringify({
-                name: this.state.name,
-                email: this.state.email,
-                password: this.state.password,
-                shippingStreet: this.state.shippingStreet,
-                shippingCity: this.state.shippingCity,
-                shippingState: this.state.shippingState,
-                shippingZip: this.state.shippingZip,
+                name: this.state.user.name,
+                email: this.state.user.email,
+                password: this.state.user.password,
+                shippingStreet: this.state.user.shippingStreet,
+                shippingCity: this.state.user.shippingCity,
+                shippingState: this.state.user.shippingState,
+                shippingZip: this.state.user.shippingZip,
             }),
             method: "POST",
             headers: {
@@ -31,7 +44,7 @@ class App extends React.Component{
                 localStorage.setItem("token", response.token)
                 // localStorage.token = response.data.token;
                 this.setState({
-                    isLoggedIn: true,
+                    user: {isLoggedIn: true,
                     name: '',
                     email: '',
                     password: '',
@@ -39,6 +52,7 @@ class App extends React.Component{
                     shippingCity: '',
                     shippingState: '',
                     shippingZip: '',
+                }
                 })
             })
         }
@@ -92,10 +106,12 @@ class App extends React.Component{
     }
 
     render() {
+
         return(
             <div>
-                <Header user={this.state}/>
+                <Header user={this.state.user} plants={this.state.plants}/>
                     <UserLogin 
+
                         email={this.state.email}
                         password={this.state.password}
                         name={this.state.name}
@@ -114,28 +130,32 @@ class App extends React.Component{
                         handleFormInput={this.handleFormInput}
                         handleSignUp={this.handleSignUp}
                     />
-                <Api />
+                {/* <Api /> */}
                 {/* <Plant />
                 <Show /> */}
                 <CatCard 
+                    plants={this.state.plants}
                     image="https://www.thespruce.com/thmb/qrWRABcI6K_plsLUn2cX8WS_-QE=/2358x1613/filters:fill(auto,1)/kararileysempervivum-18-crop-565dd32562e34681a627e2de84f691e1.jpg" 
                     category="Low Maintenance" 
                     info="Don't feel like you have a green thumb? This category of plants is designed for the first time plant owner, or the human that would like to have some plants in their life, but may have not had good luck keeping previous plant babies alive in the past." 
                 />
                 <CatCard 
+                    plants={this.state.plants}
                     image="https://www.almanac.com/sites/default/files/styles/opengraph/public/image_nodes/african-violet-houseplant.jpg?itok=qiMZjFZs" 
                     category="Air Purifying" 
                     info="These plants are great for purifying your home and office. If you feel as though your space has become musty and stale, then this category of plant is for you!" 
                 />
                 <CatCard 
+                    plants={this.state.plants}
                     image="https://www.jacksonandperkins.com/images/xxl/27677.jpg" 
                     category="Pet Loving" 
                     info="No more worries about which plants are safe for your furry family members. This plant category is all safe for plants to nom on." 
                 />
                 <CatCard 
+                    plants={this.state.plants}
                     image="https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Pygmy_Date_Palm_450_1_grande.jpg?v=1549683425" 
                     category="Mood Boosting" 
-                    into="If cabin fever is giving you the blues, pick a plant from this category and let the good vibes flow." 
+                    info="If cabin fever is giving you the blues, pick a plant from this category and let the good vibes flow." 
                 />
                 <Footer />
             </div>
