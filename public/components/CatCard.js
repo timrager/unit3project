ddcf92e5cad4fa21b82b72a26ee247b030fdb4c9
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-03 14:04:28 -0400

    Making notes about how to create image links to ask during class

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -3,6 +3,9 @@ class CatCard extends React.Component {
     render () {
         return (
             <div className="plantBox">
+                {/* Would I use an href tag here or a Link tag? */}
+                {/* Would I put the onClick function here or in the App Component */}
+                {/* <a href={}><img src={this.props.image} onClick={} /></a> */}
                 <img className="plantMenu" src={this.props.image} />
                 <h3>{this.props.category}</h3>
                 <p>{this.props.info}</p>
