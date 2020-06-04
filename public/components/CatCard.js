c154523a9c21672a23aa47144d89314badf4351a
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-04 17:55:16 -0400

    The Home page is working!

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -1,10 +1,11 @@
 
 class CatCard extends React.Component {
     render () {
+        const {category, image} = this.props.card;
         return (
             <div className="plantBox">
-                <Link to={`/${this.props.category}`} ><img className="plantMenu" src='https://cdn.shopify.com/s/files/1/0212/1030/0480/products/Mona-Lisa-Lipstick-Plant-Hanging-Basket.jpg?v=1577197824' /></Link>
-                <h3>{this.props.category}</h3>
+                <Link to={`/${category}`} ><img className="plantMenu" src={image} /></Link>
+                <h3>{category}</h3>
             </div>
         )
     }
