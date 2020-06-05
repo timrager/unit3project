2738e3fe1be8871ce943a8c48738c021fb8cf143
Katie <katie@katies-air.lan>
2020-06-04 22:08:44 -0400

    adding styling the home page

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -5,7 +5,7 @@ class CatCard extends React.Component {
         return (
             <div className="plantBox">
                 <Link to={`/${category}`} ><img className="plantMenu" src={image} /></Link>
-                <h3>{category}</h3>
+                <h3 className="plantMenu">{category}</h3>
             </div>
         )
     }
