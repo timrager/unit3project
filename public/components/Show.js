fee459a46e6c64c9c959d4d12e95ac5916fbba1a
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-05 17:14:28 -0400

    Just pulled changes from the dev branch

--- public/components/Show.js
+++ public/components/Show.js
@@ -1,6 +1,6 @@
 class Show extends React.Component {
     render () {
-        // const {name, image, plantCare, size, type} = this.props.singlePlant;
+        // const {name, image, type, plantCare, size} = this.props.singlePlant;
         console.log(this.props.singlePlant.image);
         return (
             <div>
@@ -10,7 +10,7 @@ class Show extends React.Component {
                 <div id="info-div">
                     <h2>{this.props.singlePlant.name}</h2>
                     <h3>Plant Type</h3>
-                    <h3>{this.props.singlePlant.type}</h3>
+                    <p>{this.props.singlePlant.type}</p>
                     <h3>Caring for Your Plant</h3>
                     <p>{this.props.singlePlant.plantCare.water}</p>
                     <p>{this.props.singlePlant.plantCare.sun}</p>
