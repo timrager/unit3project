c98ffa7bb9073a06a10559c234138dd8a8cb98ac
timrager <tim.rager@gmail.com>
2020-06-05 14:55:06 -0700

    updated handleChange to validate form in the console

--- public/components/NewUser.js
+++ public/components/NewUser.js
@@ -1,3 +1,6 @@
+const validEmailRegex = 
+  RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+
 class NewUser extends React.Component {
     state = {
         email: '',
@@ -15,10 +18,28 @@ class NewUser extends React.Component {
 
     handleChange = (event) =>{
         event.preventDefault();
+        const { id, value } = event.target;
+        let errors = this.state.errors;
 
+        switch (id) {
+            case 'name': 
+                errors.name = value.length < 5 ? 'Full Name must be 5 characters long!' : '';
+                break;
+            case 'email': 
+                errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
+                break;
+            case 'password': 
+                errors.password = value.length < 8 ? 'Password must be 8 characters long!' : '';
+                break;
+            default:
+                break;
+          }
 
         this.setState({
+            errors,
             [event.target.id]: event.target.value
+        }, () => {
+            console.log(errors)
         })
     }
 
@@ -42,17 +63,17 @@ class NewUser extends React.Component {
             <form onSubmit={this.createUser}>
                 <h2>Create a Profile</h2>
                 <label htmlFor="name">Name</label>
-                    <input type="text" id="name" value={this.name} onChange={this.updateName} />
+                    <input type="text" id="name" value={this.name} onChange={this.handleChange} />
                 <label htmlFor="email">Email</label>    
-                    <input type="text" id="email" value={this.email} onChange={this.updateEmail} />
+                    <input type="text" id="email" value={this.email} onChange={this.handleChange} />
                 <label htmlFor="password">Password</label>
-                    <input type="password" id="password" value={this.password} onChange={this.updatePassword} />
+                    <input type="password" id="password" value={this.password} onChange={this.handleChange} />
                 <label htmlFor="shippingStreet">Street Address</label>
-                    <input type="text" id="shippingStreet" value={this.shippingStreet} onChange={this.updateShippingStreet} />
+                    <input type="text" id="shippingStreet" value={this.shippingStreet} onChange={this.handleChange} />
                 <label htmlFor="shippingCity">City</label>
-                    <input type="text" id="shippingCity" value={this.shippingCity} onChange={this.updateShippingCity} />
+                    <input type="text" id="shippingCity" value={this.shippingCity} onChange={this.handleChange} />
                 <label htmlFor="shippingZip">Zip Code</label>
-                    <input type="text" id="shippingZip" value={this.shippingZip} onChange={this.updateShippingZip} />
+                    <input type="text" id="shippingZip" value={this.shippingZip} onChange={this.handleChange} />
                     <input type="submit"/>
             </form>
         </div>
