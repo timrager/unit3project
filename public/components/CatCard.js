2f59c3bb118692d1616b2ae13b8452d22ef859a1
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-06-04 20:32:09 -0400

    I got the Category names to appear as Title Case without the dash in between them.

--- public/components/CatCard.js
+++ public/components/CatCard.js
@@ -2,10 +2,14 @@
 class CatCard extends React.Component {
     render () {
         const {category, image} = this.props.card;
+        const fixCat = (category) => {
+            let realCat = category.replace('-', ' ').replace(/(?:^|\s)\S/g, a => a.toUpperCase());
+            return realCat;
+        }
         return (
             <div className="plantBox">
                 <Link to={`/${category}`} ><img className="plantMenu" src={image} /></Link>
-                <h3>{category}</h3>
+                <h3>{fixCat(category)}</h3>
             </div>
         )
     }
