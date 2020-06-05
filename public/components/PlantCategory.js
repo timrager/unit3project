84fd3f2eb75cadd2e063b5b05df25447a3bbc223
Whitney Smith <whitney.smith.lee@gmail.com>
2020-06-04 21:11:44 -0700

    updates to cart path

--- public/components/PlantCategory.js
+++ public/components/PlantCategory.js
@@ -4,22 +4,17 @@ class PlantCategory extends React.Component {
         console.log(location)
 
         return (
-            <div className="plantCat">
-                {this.props.plantData.map((plant) => {
-                    return (
-                        <div>
-                            <h1>{plant.name}</h1>
-                            <Link to={`/${this.props._id}`} ><img className="plantMenu" src={plant.image} /></Link>
-                            {/* <p>{plant.plantCare.water}</p>
-                            <p>{plant.plantCare.sun}</p> */}
-                            <button className="cartButton" onClick={() => shoppingCart.push(plant)}>Add to Cart</button>
-                            {console.log(shoppingCart)}
-                        </div>
-                        )
-                        // Need a show route in App
-                    // Add a component here to show current item
-                })}
+            <div>
+                <h1>{plant.name}</h1>
+                <Link to={`/${this.props._id}`} ><img className="plantMenu" src={plant.image} /></Link>
+                {/* <p>{plant.plantCare.water}</p>
+                <p>{plant.plantCare.sun}</p> */}
+                <button className="cartButton" onClick={() => shoppingCart.push(plant)}>Add to Cart</button>
+                {console.log(shoppingCart)}
             </div>
+            )
+            // Need a show route in App
+        // Add a component here to show current item
         )
     }
 }
